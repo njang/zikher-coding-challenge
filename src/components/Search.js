edcbc4532f95aecc0e6fdccb335e2a8829f59813
njang <neo.w.jang@gmail.com>
2018-03-30 00:56:30 -0500

    Add Search component that displays query

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
  
 class Search extends Component {
+	constructor(){
+		super()
+		this.state = {
+			query: ""
+		}
+	} 
+	handleInputChange = () => {
+		this.setState({
+			query: this.search.value
+		})
+	}
+
 	render() {
 		return (
-			<div className='container'>
-				<input type="text" placeholder="Search.." />
+			<div className="container">
+				<form>
+					<input 
+						type="text" placeholder="Search.." 
+						ref={input => this.search = input}
+         				onChange={this.handleInputChange}
+					/>
+					<input type="submit" text="Submit" />
+					<p>{this.state.query}</p>
+				</form>
 			</div>
 		);
 	}
 }
- 
+
 export default Search;
