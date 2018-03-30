3ccdb427e777c70168835bd5fa1a16b5c739fa12
njang <neo.w.jang@gmail.com>
2018-03-30 17:58:32 -0500

    Add Ribbon to show GitHub link

---
+++ src/components/Ribbon.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+ 
+class Ribbon extends Component {
+	render() {
+		return (
+			<div className='ribbon'>
+				<a href="https://github.com/njang/zikher-coding-challenge" target="_blank">View Source on GitHub</a>
+			</div>
+		);
+	}
+}
+ 
+export default Ribbon; 
