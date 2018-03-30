0b43f7bbb737fd5a244782976b5543f995039974
njang <neo.w.jang@gmail.com>
2018-03-30 18:39:24 -0500

    Refactor Main

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import Search from "./components/Search";
 import Ribbon from "./components/Ribbon";
+import Search from "./components/Search";
 import Footer from "./components/Footer";
 
 class Main extends Component {
-  render() {
-    return (
-      <div className="Main">
-        {/*<h1 className="Main-title text-center">Zikher Coding Challenge</h1>*/}
-        <Ribbon />
-        <Search />
-        <Footer />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="Main">
+                {/*<h1 className="Main-title text-center">Zikher Coding Challenge</h1>*/}
+                <Ribbon />
+                <Search />
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default Main;
