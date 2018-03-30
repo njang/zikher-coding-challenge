93ce1c212997596ceb2ae081c62790591a564129
njang <neo.w.jang@gmail.com>
2018-03-30 12:35:33 -0500

    Parse the response data

--- src/components/Search.js
+++ src/components/Search.js
@@ -16,22 +16,30 @@ class Search extends Component {
 
 	handleSubmit = event => {
     	event.preventDefault();
-	    const user = {
-	      	query: this.state.query
-	    };
-	    axios.get(`${API_URL}?q=${this.state.query}`, { user })
+	    axios.get(`${API_URL}?q=${this.state.query}`)
 	    	.then(res => {
 	        console.log(res.data.items);
+	        this.setState({
+	        	results: res.data.items
+	        })
       })
 	}
 
 	render() {
+		let resultDisplay = this.state.results.map((book) => {
+            return (
+                <li key={book.id}>{ book.volumeInfo.title } by { book.volumeInfo.authors[0] }</li>
+            )
+        })
 		return (
 			<div className="container">
 				<form onSubmit={this.handleSubmit}>
 	            	<input type="text" name="query" onChange={this.handleChange} />
 	          		<button type="submit">Find</button>
         		</form>
+        		<ul>
+    			{ resultDisplay }
+    			</ul>
 			</div>
 		);
 	}
