3a53ba48a4a3151e8c15c1791fee51551c8e72a2
njang <neo.w.jang@gmail.com>
2018-03-30 17:58:52 -0500

    Add Ribbon component in Main

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Search from "./components/Search";
+import Ribbon from "./components/Ribbon";
 import Footer from "./components/Footer";
 
 class Main extends Component {
   render() {
     return (
       <div className="Main">
-        <h1 className="Main-title">Zikher Coding Challenge</h1>
+        <h1 className="Main-title text-center">Zikher Coding Challenge</h1>
+        <Ribbon />
         <Search />
         <Footer />
       </div>
