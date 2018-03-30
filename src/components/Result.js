3c4d1ba72fa2faf4d7ea149686fae32ea89a20d3
njang <neo.w.jang@gmail.com>
2018-03-30 15:52:19 -0500

    Enable react-table

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
- 
+import ReactTable from 'react-table'
+import 'react-table/react-table.css'
+
 class Result extends Component {
 	render() {
 		return (
 			<div className='container'>
-				Search results
-				<ul>
-    				{this.props.results.map(book => (
-		                <li key={book.id}>{ book.volumeInfo.title }</li>
-        			))
-    				}
-    			</ul>
+    			<ReactTable
+					data= {this.props.results}
+					columns={[
+						{
+							Header: "Title",
+							accessor: "volumeInfo.title"
+						},
+						{
+							Header: "Authors",
+							accessor: "volumeInfo.authors"
+						},
+						{
+							Header: "Published",
+							accessor: "volumeInfo.publishedDate"
+						}	
+					]}
+				/>
 			</div>
 		);
 	}
