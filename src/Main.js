87a2e7fadfd6111b5983e677daf45c7c5487abec
njang <neo.w.jang@gmail.com>
2018-03-30 18:14:47 -0500

    Hide title header

--- src/Main.js
+++ src/Main.js
@@ -7,7 +7,7 @@ class Main extends Component {
   render() {
     return (
       <div className="Main">
-        <h1 className="Main-title text-center">Zikher Coding Challenge</h1>
+        {/*<h1 className="Main-title text-center">Zikher Coding Challenge</h1>*/}
         <Ribbon />
         <Search />
         <Footer />
