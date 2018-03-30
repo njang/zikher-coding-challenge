67c91a2122ed6de023286f1555b0f6d5ac5353d5
njang <neo.w.jang@gmail.com>
2018-03-30 13:43:47 -0500

    Relocate component call for Result

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Search from "./components/Search";
-import Result from "./components/Result";
 import Footer from "./components/Footer";
 
 class Main extends Component {
@@ -9,7 +8,6 @@ class Main extends Component {
       <div className="Main">
         <h1 className="Main-title">Zikher Coding Challenge</h1>
         <Search />
-        <Result />
         <Footer />
       </div>
     );
