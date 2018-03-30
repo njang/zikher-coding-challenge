88173baf220cbbd50b8d92b46f31e758ac8a92fd
njang <neo.w.jang@gmail.com>
2018-03-29 23:00:42 -0500

    Add Search and Footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+ 
+class Footer extends Component {
+	render() {
+		return (
+			<footer className='py-3 bg-dark'>
+				<p className='m-0 text-center text-white'>Handcrafted with <span className='text-white'>&#10084;</span></p>
+				<p className='m-0 text-center text-white'>&copy; Neo W. Jang 2017-2018</p>
+			</footer>
+		);
+	}
+}
+ 
+export default Footer; 
