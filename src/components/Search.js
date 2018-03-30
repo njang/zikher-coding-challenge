839e60cd52cfaf6eb0a143e7b1ecd6fa8b88a8be
njang <neo.w.jang@gmail.com>
2018-03-30 18:39:58 -0500

    Enable separate rendering before and after response

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,6 +8,7 @@ class Search extends Component {
 	state = {
 		query: "",
 		limit: "40",
+		initialMode: true,
 		results: []
 	}
 
@@ -19,23 +20,40 @@ class Search extends Component {
 
 	handleSubmit = event => {
     	event.preventDefault();
-	    axios.get(`${API_URL}?q=${this.state.query}&maxResults=${this.state.limit}`).then((res) => {
+    	axios.get(`${API_URL}?q=${this.state.query}&maxResults=${this.state.limit}`).then((res) => {
 	        this.setState({
+	        	initialMode: false,
 	        	results: res.data.items
 	        })
-        })
+        });
 	}
 
 	render() {
-		return (
-			<div className="container-fluid">
-                <div className="row align-items-center justify-content-center search">
+		let displayElement = (
+			<div className="row align-items-center justify-content-center search">
+				<form className="col-11 col-sm-8 row form-group justify-content-center" onSubmit={this.handleSubmit}>
+            		<input className="col-9 col-md-8 col-lg-7 form-control" type="text" name="query" aria-label="Query" placeholder="Search Google Books" onChange={this.handleChange} />
+            		{/*<input className="form-control" type="number" name="limit" aria-label="Limit" placeholder="Limit results" defaultValue="10" step="5" min="10" max="40" onChange={this.handleChange} />*/}
+	          		<button className="col-3 col-md-2 col-lg-1 btn" type="submit">Find</button>
+        		</form>
+    		</div>
+		);
+		console.log(displayElement);
+		if (this.state.initialMode === false) {
+			displayElement = (
+				<div className="row align-items-center justify-content-center result">
 					<form className="col-11 col-sm-8 row form-group justify-content-center" onSubmit={this.handleSubmit}>
 	            		<input className="col-9 col-md-8 col-lg-7 form-control" type="text" name="query" aria-label="Query" placeholder="Search Google Books" onChange={this.handleChange} />
 	            		{/*<input className="form-control" type="number" name="limit" aria-label="Limit" placeholder="Limit results" defaultValue="10" step="5" min="10" max="40" onChange={this.handleChange} />*/}
 		          		<button className="col-3 col-md-2 col-lg-1 btn" type="submit">Find</button>
 	        		</form>
-        		</div>
+	        		<Result results={this.state.results} />
+	    		</div>
+			);
+		}
+		return (
+			<div className="container-fluid">
+          		{ displayElement }      
 			</div>
 		);
 	}
