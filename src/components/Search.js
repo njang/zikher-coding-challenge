97c533cf0ab8b425a8fc7f9094427004fdeb1765
njang <neo.w.jang@gmail.com>
2018-03-30 22:34:24 -0500

    Revise styling

--- src/components/Search.js
+++ src/components/Search.js
@@ -32,13 +32,12 @@ class Search extends Component {
 		let displayElement = (
 			<div className="row align-items-center justify-content-center search">
 				<form className="col-11 col-sm-8 row form-group justify-content-center" onSubmit={this.handleSubmit}>
-            		<input className="col-9 col-md-8 col-lg-7 form-control" type="text" name="query" aria-label="Query" placeholder="Search Google Books" onChange={this.handleChange} />
+            		<input className="col-9 col-md-8 form-control" type="text" name="query" aria-label="Query" placeholder="Search Google Books" onChange={this.handleChange} />
             		{/*<input className="form-control" type="number" name="limit" aria-label="Limit" placeholder="Limit results" defaultValue="10" step="5" min="10" max="40" onChange={this.handleChange} />*/}
-	          		<button className="col-3 col-md-2 col-lg-1 btn" type="submit">Find</button>
+	          		<button className="col-3 col-md-2 btn" type="submit">Find</button>
         		</form>
     		</div>
 		);
-		console.log(displayElement);
 		if (this.state.initialMode === false) {
 			displayElement = (
 				<div className="row align-items-center justify-content-center result">
